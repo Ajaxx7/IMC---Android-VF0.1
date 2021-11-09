a859c9560e1ce58a779c4b0187556136aba7e5ca
Ajaxx7 <gustavo.galvao.agostinho@gmail.com>
2021-11-09 16:53:03 -0300

    Aoba fi

--- app/src/main/java/com/example/imcapp_final/MainActivity.kt
+++ app/src/main/java/com/example/imcapp_final/MainActivity.kt
@@ -1,13 +1,30 @@
 package com.example.imcapp_final
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Remover a toolbar
         supportActionBar!!.hide()
+
+        //Instanciar o botão entrar
+        val botaoEntrar = findViewById<Button>(R.id.button_criar_conta)
+
+        botaoEntrar.setOnClickListener {
+            //Criar uma intent(intenção)
+            val abrirCadastro = Intent (this, cadastro_1::class.java)
+            startActivity(abrirCadastro)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return super.onCreateOptionsMenu(menu)
     }
 }
---
+++ app/src/main/java/com/example/imcapp_final/cadastro_1.kt
@@ -0,0 +1,13 @@
+package com.example.imcapp_final
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+
+class cadastro_1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_1)
+    }
+    
+}
